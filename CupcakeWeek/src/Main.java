a75d4473831c77011350f2e640a8cfe824e6c5b7
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-06-05 18:10:04 -0400

    Update - Project 1: Step 4 and 5 complete; added code to support exceptions during user input.

--- CupcakeWeek/src/Main.java
+++ CupcakeWeek/src/Main.java
@@ -1,10 +1,112 @@
-import java.sql.SQLOutput;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Creates a new ArrayList for the cupcakeMenu, which is a list.
+        List<Cupcake> cupcakeMenu = new ArrayList<Cupcake>();
 
+        // Object initialization for our ArrayList.
+        Cupcake cupcake = new Cupcake();
+        RedVelvet redVelvet = new RedVelvet();
+        Chocolate chocolate = new Chocolate();
+
+        // Start of initializing the prices of each cupcake.
+        System.out.println("We are in the middle of creating the cupcake menu. We currently have three cupcakes on the menu but we need to decide on pricing.");
+
+        // Initializing scanner.
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nWe are deciding on the price for our standard cupcake. Here is the description:");
+
+        // Printing out cupcake's description.
+        cupcake.type();
+
+        // Grabbing user's price input for the ordinary cupcake.
+        System.out.print("\nHow much would you like to charge for the cupcake? (Input a numerical number taken to 2 decimal places) ");
+
+        // Variables are put outside the while loop to be reused.
+        String priceText;
+        double price;
+
+        // Confirming what was inputted is usable and converting it to a usable double.
+        while (true) {
+            priceText = input.nextLine();
+            try {
+                price = Double.parseDouble(priceText);
+
+                // If the parsing does not throw an error, this formats the double into a string that has only two decimal places.
+                priceText = String.format("%.2f", price);
+                System.out.println("You have set the price for the regular cupcake as $" + cupcake.getPrice());
+                break;
+            }
+
+            // Since we are grabbing the price in a string, this is necessary to make sure we get an input that makes sense.
+            catch (NumberFormatException n){
+                System.out.print("\nYou entered value that isn't a proper price. Please try again: ");
+            }
+        }
+
+        // Setting the price for cupcakes.
+        cupcake.setPrice(Double.parseDouble(priceText));
+
+        // Grabbing user's price input for the red velvet cupcake.
+        System.out.print("\nHow much would you like to charge for the red velvet cupcake? (Input a numerical number taken to 2 decimal places) ");
+
+        // Confirming what was inputted is usable and converting it to a usable double.
+        while (true) {
+            priceText = input.nextLine();
+            try {
+                price = Double.parseDouble(priceText);
+
+                // If the parsing does not throw an error, this formats the double into a string that has only two decimal places.
+                priceText = String.format("%.2f", price);
+                System.out.println("You have set the price for the red velvet cupcake as $" + redVelvet.getPrice());
+                break;
+            }
+
+            // Since we are grabbing the price in a string, this is necessary to make sure we get an input that makes sense.
+            catch (NumberFormatException n){
+                System.out.print("\nYou entered value that isn't a proper price. Please try again: ");
+            }
+        }
+
+        // Setting the price for red velvet cupcakes.
+        redVelvet.setPrice(Double.parseDouble(priceText));
+
+        // Grabbing user's price input for the chocolate cupcake.
+        System.out.print("\nHow much would you like to charge for the chocolate cupcake? (Input a numerical number taken to 2 decimal places) ");
+
+        // Confirming what was inputted is usable and converting it to a usable double.
+        while (true) {
+            priceText = input.nextLine();
+            try {
+                price = Double.parseDouble(priceText);
+
+                // If the parsing does not throw an error, this formats the double into a string that has only two decimal places.
+                priceText = String.format("%.2f", price);
+                System.out.println("You have set the price for the chocolate cupcake as $" + chocolate.getPrice());
+                break;
+            }
+
+            // Since we are grabbing the price in a string, this is necessary to make sure we get an input that makes sense.
+            catch (NumberFormatException n){
+                System.out.print("\nYou entered value that isn't a proper price. Please try again: ");
+            }
+        }
+
+        // Setting the price for red velvet cupcakes.
+        chocolate.setPrice(Double.parseDouble(priceText));
+
+        // Adding cupcake objects to ArrayList.
+        cupcakeMenu.add(cupcake);
+        cupcakeMenu.add(redVelvet);
+        cupcakeMenu.add(chocolate);
     }
 }
+
+// Cupcake class - the Parent class.
 class Cupcake {
     public double price;
 
@@ -21,6 +123,7 @@ class Cupcake {
     }
 }
 
+// RedVelvet class - a child class of Cupcake.
 class RedVelvet extends Cupcake {
     @Override
     public void type() {
@@ -28,6 +131,7 @@ class RedVelvet extends Cupcake {
     }
 }
 
+// Chocolate class - a child class of Cupcake.
 class Chocolate extends Cupcake {
     @Override
     public void type() {
