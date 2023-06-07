3c81e4914e66cfb24d0216e15d49d2fbccf6b9ce
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-06-07 13:17:19 -0400

    Update - Project 3: Step 3 Complete

--- CupcakeWeek/src/Order.java
+++ CupcakeWeek/src/Order.java
@@ -1,4 +1,5 @@
-import java.sql.SQLSyntaxErrorException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,13 +12,43 @@ public class Order {
         Scanner input = new Scanner(System.in);
         System.out.print("\nHello valued customer. Would you like to place an order? (Y/N)");
 
-        String placeOrder = input.nextLine();;
+        String placeOrder = input.nextLine();
 
         ArrayList<Object> order = new ArrayList<>();
 
         // Confirming user input.
         if (placeOrder.equalsIgnoreCase("y")) {
+            // Step 3
+            order.add(LocalDate.now());
+            order.add(LocalTime.now());
 
+            System.out.println("Here is the menu:\n\tCupcakes:");
+
+            int itemNumber = 0;
+
+            for (Cupcake cupcake : cupcakeMenu) {
+                itemNumber++;
+
+                System.out.print(itemNumber + ": ");
+                cupcake.type();
+
+                System.out.print("Price: $" + cupcake.getPrice());
+
+                System.out.println();
+            }
+
+            System.out.println("\tDrinks:");
+
+            for (Drink drink : drinkMenu) {
+                itemNumber++;
+
+                System.out.print(itemNumber + ": ");
+                drink.type();
+
+                System.out.print("Price: $" + drink.getPrice());
+
+                System.out.println();
+            }
         } else if (placeOrder.equalsIgnoreCase("n")) {
             System.out.println("\nHave a nice day then.");
         } else {
