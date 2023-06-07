817efb161a0629e32c944dda06f580cf215648fb
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-06-07 13:35:15 -0400

    Update - Project 3: Step 4 Complete

--- CupcakeWeek/src/Order.java
+++ CupcakeWeek/src/Order.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Order {
@@ -53,6 +54,74 @@ public class Order {
             System.out.println("\nHave a nice day then.");
         } else {
             System.out.println("\nAs you do not seem to speak English, please leave and have a nice day.");
+        }
+
+        // Step 4
+        boolean ordering = true;
+
+        // Added for more realistic immersion of app.
+        boolean ordered = false;
+
+        while (ordering) {
+            System.out.println("What would you like to order? Please use the number associated with each item to order.");
+            try {
+                int orderChoice = input.nextInt();
+
+                // Regular cupcake
+                if (orderChoice == 1) {
+                    order.add(cupcakeMenu.get(1));
+                    ordered = true;
+                    System.out.println("\nItem added to order.");
+
+                // Red velvet cupcake
+                } else if (orderChoice == 2) {
+                    order.add(cupcakeMenu.get(2));
+                    ordered = true;
+                    System.out.println("\nItem added to order.");
+
+                // Chocolate cupcake
+                } else if (orderChoice == 3) {
+                    order.add(cupcakeMenu.get(3));
+                    ordered = true;
+                    System.out.println("\nItem added to order.");
+
+                // Water
+                } else if (orderChoice == 4) {
+                    order.add(drinkMenu.get(1));
+                    ordered = true;
+                    System.out.println("\nItem added to order.");
+
+                // Soda
+                } else if (orderChoice == 5) {
+                    order.add(drinkMenu.get(2));
+                    ordered = true;
+                    System.out.println("\nItem added to order.");
+
+                // Milk
+                } else if (orderChoice == 6) {
+                    order.add(drinkMenu.get(3));
+                    ordered = true;
+                    System.out.println("\nItem added to order.");
+                } else {
+                    System.out.println("\nPlease enter a number from above; we do not have your selected item on the menu.");
+                }
+            }
+            catch (InputMismatchException i) {
+                System.out.println("\nPlease enter only a number.");
+            }
+
+            System.out.print("\nWould you like to continue order? (Y/N)");
+
+            placeOrder = input.nextLine();
+            if (!placeOrder.equalsIgnoreCase("y") && ordered) {
+                System.out.println("\nThank you for your order");
+                ordering = false;
+
+            // For when the customer didn't order anything and only window shopped.
+            } else if (!placeOrder.equalsIgnoreCase("y") && !ordered){
+                System.out.println("\nThank you for visiting us, and have a nice day,");
+                ordering = false;
+            }
         }
     }
 }
